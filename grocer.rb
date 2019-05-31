d7cbc1a0dd2b81fc9bb83fb19a667d8b5af32c3d
Justin Hutsell <hutselljustin@gmail.com>
2019-05-31 18:01:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,26 @@
 def consolidate_cart(cart)
   # code here
+  organized_cart = {}
+  
+  cart.each do |groceries|
+    groceries.each do |item, details|
+      if !organized_cart.has_key?(item)
+        organized_cart[item] = details
+        organized_cart[item][:count] = 1 
+      else
+        organized_cart[item][:count] += 1 
+      end
+    end
+  end
+  organized_cart
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  coupons.each do |coupon|
+    name = coupon[:item]
+    
+  end
 end
 
 def apply_clearance(cart)
