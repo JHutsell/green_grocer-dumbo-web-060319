08c4e46c1a86655e4321831e058300aa2a58083a
Justin Hutsell <hutselljustin@gmail.com>
2019-05-31 18:09:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,5 +49,8 @@ def checkout(cart, coupons)
   coupon_cart = apply_coupons(consolidated_cart, coupons)
   final_cart = apply_clearance(coupon_cart)
   total = 0 
-  
+  final_cart.each do |name, properties|
+    total += 
+    
+  end
 end
